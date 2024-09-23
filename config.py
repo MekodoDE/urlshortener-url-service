4549eb7676f71d19011045c55b53486434bcbf22
Marco <Marco@TOKYO>
2024-09-23 22:28:43 +0200

    Init JWT_ACCESS_TOKEN_EXPIRES for token duration

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-from datetime import timedelta
-
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -7,10 +5,11 @@ class Config(object):
     API_TITLE = 'url-service'
     OPENAPI_VERSION = '3.0.2'
     OPENAPI_URL_PREFIX = '/'
+    URL_PREFIX = '/urls/'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///urls.db'
     CORS_ORIGINS = ["*"]
     JWT_SECRET_KEY = "super-secret"
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1024)
+    JWT_ACCESS_TOKEN_EXPIRES_MINUTES = 1440
     URL_KEY_LENGTH = 3
 class ProductionConfig(Config):
     DATABASE_URI = 'mysql://user@127.0.0.1/urls'
